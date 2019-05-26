3197008c1aaf13dcb9761dedf4271b2a7c86c5ef
Dan Ross <dan@rosspixelworks.com>
2019-05-25 21:56:44 -0500

    Ignore exports directory. Successfully cloning repo into exports.

--- glbackup.py
+++ glbackup.py
@@ -1,9 +1,30 @@
 import gitlab
+import git
+import os
 
 
 gl = gitlab.Gitlab.from_config("default", [".config"])
 
 projects = gl.projects.list(owned=True, all=True)
 
-for project in projects:
-    print(project.name)
+p = gl.projects.get(projects[0].id)
+
+g = git.Git()
+os.chdir("exports")
+g.clone(p.ssh_url_to_repo)
+# g.pull()
+# p.ssh_url_to_repo
+# export = p.exports.create({})
+
+
+# # Wait for the 'finished' status
+# export.refresh()
+# while export.export_status != 'finished':
+#     time.sleep(1)
+#     export.refresh()
+
+# # Download the result
+# with open(f'{projects[0].name}-export.tgz', 'wb') as f:
+#     export.download(streamed=True, action=f.write)
+# # for project in projects:
+#     # p = gl.projects.get(project.id)
